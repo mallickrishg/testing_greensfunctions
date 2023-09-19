cc23c6ef1791500087e986db93faab778c058239
Rishav Mallick <mallickrishg@gmail.com>
2023-09-19 13:50:51 -0700

    use tensorprod() for N-D matrix multiplication

--- antiplane/PLOT_multiplesources_lineigenstrain.m
+++ antiplane/PLOT_multiplesources_lineigenstrain.m
@@ -10,17 +10,16 @@ mu = 1;
 Nsources = 2;
 alpha_0 = 1;
 alpha_1 = 1;
-sources = [1,1;0,0];
 
 % dimensions of source
 % specify location and dimensions of cources
-xloc = [-1,1]*0.5;
+xloc = [-1,1]*1;
 yloc = ones(Nsources,1).*0;
-Rx = ones(Nsources,1).*0.5;
+Rx = ones(Nsources,1).*1;
 Ry = ones(Nsources,1).*1.5;
 
 % discretize evaluation points
-nx = 100;
+nx = 200;
 ny = nx;
 
 x_vec = linspace(-4, 4, nx);
@@ -41,45 +40,40 @@ for i = 1:Nsources
 end
 
 %% plot displacement and stresses
-u1 = zeros(nx*ny,1);
-s12 = zeros(nx*ny,1);
-s13 = zeros(nx*ny,1);
 
 alpha0_vec = [1,1];
-alpha1_vec = [0,0];
+alpha1_vec = [1,-1];
 sources = [alpha0_vec;alpha1_vec];
 
-for i = 1:Nsources
-    u1 = u1 + Ku(:,:,i)*sources(:,i);
-    s12 = s12 + Ks12(:,:,i)*sources(:,i);
-    s13 = s13 + Ks13(:,:,i)*sources(:,i);
-end
-
+% use tensor products to contract 3-d matrices
+u1 = tensorprod(Ku,sources,[2 3],[1 2]);
+s12 = tensorprod(Ks12,sources,[2 3],[1 2]);
+s13 = tensorprod(Ks13,sources,[2 3],[1 2]);
 
 figure(1),clf
 pcolor(x_vec,y_vec,reshape(u1,ny,nx)), shading interp, hold on
-contour(x_vec,y_vec,reshape(u1,ny,nx),linspace(-1,1,21).*max(abs(u1(:))),'k-')
+contour(x_vec,y_vec,reshape(u1,ny,nx),linspace(-1,1,11).*max(abs(u1(:))),'k-')
 axis tight equal
-colorbar
+cb=colorbar; cb.Location='northoutside';
 clim([-1 1].*max(abs(u1(:))))
-colormap bluewhitered(20)
+colormap bluewhitered(1000)
 
 figure(2),clf
-subplot(2,1,1)
+subplot(1,2,1)
 pcolor(x_vec,y_vec,reshape(s12,ny,nx)), shading interp, hold on
 contour(x_vec,y_vec,reshape(s12,ny,nx),linspace(-1,1,21).*max(abs(s12(:))),'k-')
 axis tight equal
 clim([-1 1].*max(abs(s12(:))))
 colorbar
 
-subplot(2,1,2)
+subplot(1,2,2)
 pcolor(x_vec,y_vec,reshape(s13,ny,nx)), shading interp, hold on
 contour(x_vec,y_vec,reshape(s13,ny,nx),linspace(-1,1,21).*max(abs(s13(:))),'k-')
 axis tight equal
 clim([-1 1].*max(abs(s13(:))))
 colorbar
 
-colormap bluewhitered(20)
+colormap bluewhitered(1000)
 
 figure(11),clf
 toplot = reshape(u1,ny,nx);
