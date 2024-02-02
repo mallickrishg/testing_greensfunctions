4d81b41e04d3d3b83e20af0add162639787e5820
Rishav Mallick <mallickrishg@gmail.com>
2024-02-02 14:28:48 -0800

    modified plotting

--- antiplane/heteromaterial/EXPT_thinplate_bodyforcesol.m
+++ antiplane/heteromaterial/EXPT_thinplate_bodyforcesol.m
@@ -7,18 +7,19 @@
 clear
 
 % topo mesh
-Ltopo = 40;
-Ntopo = Ltopo*37;
+Ltopo = 50;
+Ntopo = Ltopo*20;
+Lthickness = 2;
 
 % construct source
-rcv = create_verticalinterfaces(-2,-0.5,2,0);
+rcv = create_verticalinterfaces(-Lthickness,-Lthickness/5,2,0);
 
 % construct free surface
-topo = create_horizontalinterfaces(-Ltopo/2,Ltopo/2,Ntopo+1,[0,-2]);
+topo = create_horizontalinterfaces(-Ltopo/2,Ltopo/2,Ntopo+1,[0,-Lthickness]);
 
 %% compute source term at the interfaces
 source = ones(rcv.N,1);
-deltashift = 1e-5.*[-ones(topo.N/2,1);ones(topo.N/2,1)];
+deltashift = 1e-9.*[-ones(topo.N/2,1);ones(topo.N/2,1)];
 
 [Disp,Dgradient] = compute_disp_stress_kernels_fault(rcv,[topo.x2c + deltashift.*topo.nvec(:,1),...
                                                           topo.x3c + deltashift.*topo.nvec(:,2)]);
@@ -39,7 +40,7 @@ bemsol = -Kuz\uz0;
 %% surface and bulk displacements
 
 Nobs = 100;
-obs = [linspace(-2,2,Nobs)',zeros(Nobs,1)];
+obs = [linspace(-2*Lthickness,2*Lthickness,Nobs)',zeros(Nobs,1)];
 
 % source contribution
 [Disp,~] = compute_disp_stress_kernels_fault(rcv,obs);
@@ -51,23 +52,23 @@ uplot_bem = Ku*bemsol;
 
 figure(2),clf
 subplot(2,1,1)
-plot(obs(:,1),uplot_0,'-','LineWidth',4), hold on
+plot(obs(:,1)./Lthickness,uplot_0,'-','LineWidth',4), hold on
 axis tight
-xlabel('x'), ylabel('displacement')
+xlabel('x/L'), ylabel('displacement')
 ylim([-1 1]*0.5)
 set(gca,'FontSize',15,'Linewidth',1)
 
 subplot(2,1,2)
-plot(obs(:,1),uplot_0 + uplot_bem,'k-','LineWidth',3)
-xlabel('x'), ylabel('displacement')
+plot(obs(:,1)./Lthickness,uplot_0 + uplot_bem,'k-','LineWidth',3)
+xlabel('x/L'), ylabel('displacement')
 set(gca,'FontSize',15,'Linewidth',1)
 ylim([-1 1]*0.5)
 
 %% plot displacements in the bulk
 nx = 50;
-nz = 51;
+nz = 50;
 ox = linspace(-2,2,nx);
-oz = linspace(-2,0,nz);
+oz = linspace(-Lthickness+1e-9,-1e-9,nz);
 [xg,zg] = meshgrid(ox,oz);
 obs = [xg(:),zg(:)];
 
