67d07e9037fae509f2ccc5326426967b6227d7b4
Rishav Mallick <mallickrishg@gmail.com>
2024-03-07 11:21:26 -0800

    included lower and upper limits for integrating over a triangle

--- antiplane/Pointforce_2dintegral_antiplane.m
+++ antiplane/Pointforce_2dintegral_antiplane.m
@@ -14,14 +14,19 @@ mu = 1;
 eval_type = 1;
 
 % rectangle domain
-rectangle_x = 1.5;
-rectangle_y = 1.5;
+rectangle_x = 1;
+rectangle_y = 1;
+
+% triangle domain: (0,0),(l_x,dl_y),(0,l_y)
+l_x = 1;
+l_y = 1;
+dl_y = -0.5;
 
 nx = 100;
 ny = 100;
 
-x_vec = linspace(-3, 3, nx);
-y_vec = linspace(-3, 3, ny);
+x_vec = linspace(-2, 2, nx);
+y_vec = linspace(-2, 2, ny);
 
 if eval_type == 1
     % create a box grid of points
@@ -42,11 +47,22 @@ s12 = zeros(size(x_mat));
 s13 = zeros(size(x_mat));
 
 tic
-area_source = 0.5*rectangle_x*rectangle_y;
-Rx = rectangle_x;
-Ry = rectangle_y;
-ymax = @(x) Ry*(1 - 2*x/Rx);
+% area_source = 0.5*rectangle_x*rectangle_y;
+% Rx = rectangle_x;
+% Ry = rectangle_y;
+% Calculate the lengths of the sides of the triangle
+a = sqrt((l_x - 0)^2 + (dl_y - 0)^2);
+b = sqrt((0 - l_x)^2 + (l_y - dl_y)^2);
+c = sqrt((0 - 0)^2 + (0 - l_y)^2);
 
+% Calculate the semi-perimeter of the triangle
+s = (a + b + c) / 2;
+
+% Calculate the area of the triangle using Heron's formula
+area_source = sqrt(s * (s - a) * (s - b) * (s - c));
+
+ymax = @(x) l_y - (l_y-dl_y)*(x/l_x);
+ymin = @(x) dl_y*x/l_x;
 % fun_u = @(x,y,x0,y0) pointforce_disp(x,y,x0,y0);
 % fun_s12 = @(x,y,x0,y0) pointforce_s12(x,y,x0,y0);
 % fun_s13 = @(x,y,x0,y0) pointforce_s13(x,y,x0,y0);
@@ -56,18 +72,18 @@ parfor k=1:numel(x_mat)
     fun_s13 = @(x0,y0) pointforce_s13(x_mat(k),y_mat(k),x0,y0);
     
     % rectangle source
-    u1(k) = integral2(fun_u,-Rx/2,Rx/2,-Ry/2,Ry/2)./area_source;
-    s12(k) = mu.*integral2(fun_s12,-Rx/2,Rx/2,-Ry/2,Ry/2)./area_source;
-    s13(k) = mu.*integral2(fun_s13,-Rx/2,Rx/2,-Ry/2,Ry/2)./area_source;
+    % u1(k) = integral2(fun_u,-Rx/2,Rx/2,-Ry/2,Ry/2)./area_source;
+    % s12(k) = mu.*integral2(fun_s12,-Rx/2,Rx/2,-Ry/2,Ry/2)./area_source;
+    % s13(k) = mu.*integral2(fun_s13,-Rx/2,Rx/2,-Ry/2,Ry/2)./area_source;
 
     % triangle source
     % u1(k) = integral2(@(x0,y0) fun_u(x_mat(k),y_mat(k),x0,y0),0,Rx,-Ry,ymax)./area_source;
     % s12(k) = mu.*integral2(@(x0,y0) fun_s12(x_mat(k),y_mat(k),x0,y0),0,Rx,-Ry,ymax)./area_source;
     % s13(k) = mu.*integral2(@(x0,y0) fun_s13(x_mat(k),y_mat(k),x0,y0),0,Rx,-Ry,ymax)./area_source;
 
-    % u1(k) = integral2(fun_u,0,Rx,-Ry,ymax)./area_source;
-    % s12(k) = mu.*integral2(fun_s12,0,Rx,-Ry,ymax)./area_source;
-    % s13(k) = mu.*integral2(fun_s13,0,Rx,-Ry,ymax)./area_source;
+    u1(k) = integral2(fun_u,0,l_x,ymin,ymax)./area_source;
+    s12(k) = mu.*integral2(fun_s12,0,l_x,ymin,ymax)./area_source;
+    s13(k) = mu.*integral2(fun_s13,0,l_x,ymin,ymax)./area_source;
 
 end
 toc
@@ -79,7 +95,8 @@ if eval_type == 1
     pcolor(x_mat,y_mat,reshape(u1(:),ny,nx)),shading interp
     hold on
     contour(x_mat,y_mat,reshape(u1(:),ny,nx),9,'k-')
-    plot(rectangle_x/2.*[-1,1,1,-1,-1],rectangle_y/2.*[1,1,-1,-1,1],'k-','Linewidth',2)
+    % plot(rectangle_x/2.*[-1,1,1,-1,-1],rectangle_y/2.*[1,1,-1,-1,1],'k-','Linewidth',2)
+    plot([0,l_x,0,0],[0,dl_y,l_y,0],'k-','Linewidth',2)
     axis tight equal
     cb=colorbar;cb.Label.String = 'u';
     colormap('sky(10)')
@@ -90,7 +107,8 @@ if eval_type == 1
     pcolor(x_mat,y_mat,reshape(s12(:),ny,nx)),shading interp
     hold on
     contour(x_mat,y_mat,reshape(s12(:),ny,nx),[-1:0.1:1]*0.5,'k-')
-    plot(rectangle_x/2.*[-1,1,1,-1,-1],rectangle_y/2.*[1,1,-1,-1,1],'k-','Linewidth',2)
+    % plot(rectangle_x/2.*[-1,1,1,-1,-1],rectangle_y/2.*[1,1,-1,-1,1],'k-','Linewidth',2)
+    plot([0,l_x,0,0],[0,dl_y,l_y,0],'k-','Linewidth',1)
     axis tight equal
     cb=colorbar;cb.Label.String = '\sigma_{xz}';
     set(gca,'Fontsize',15,'LineWidth',1)
@@ -100,7 +118,8 @@ if eval_type == 1
     pcolor(x_mat,y_mat,reshape(s13(:),ny,nx)),shading interp
     hold on
     contour(x_mat,y_mat,reshape(s13(:),ny,nx),[-1:0.1:1]*0.5,'k-')
-    plot(rectangle_x/2.*[-1,1,1,-1,-1],rectangle_y/2.*[1,1,-1,-1,1],'k-','Linewidth',2)
+    % plot(rectangle_x/2.*[-1,1,1,-1,-1],rectangle_y/2.*[1,1,-1,-1,1],'k-','Linewidth',2)
+    plot([0,l_x,0,0],[0,dl_y,l_y,0],'k-','Linewidth',1)
     axis tight equal
     cb=colorbar;cb.Label.String = '\sigma_{yz}';
     % clim([-1 1]*max(abs(s13(:))))
